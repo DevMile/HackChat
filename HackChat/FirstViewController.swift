51eaa74646792dda30c89ae23b67319323e8f729
Milan Bojic <milan.bojic.developer@gmail.com>
2018-11-27 15:59:27 +0100

    Initial Commit

---
+++ HackChat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HackChat
+//
+//  Created by Milan Bojic on 11/27/18.
+//  Copyright © 2018 Milan Bojic. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
